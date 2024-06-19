b0f7900b253cdd6b77a45ef429b67ca3797a9df9
truds99 <dudscardoso9@gmail.com>
2024-06-19 19:10:09 -0300

    add post item

--- src/app.js
+++ src/app.js
@@ -5,4 +5,18 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+const items = [];
+
+app.post('/items', (req, res) => {
+    const { name, quantity, type } = req.body;
+    const item = {
+        name, 
+        quantity, 
+        type,
+        id: items.length + 1
+    }
+    items.push(item);
+    res.send(item); 
+});
+
 app.listen(5000, () => console.log('Server is running on port 5000'));
