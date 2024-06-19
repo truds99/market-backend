2f7bb2443afcd90c8350c1c39a5de1289820f677
truds99 <dudscardoso9@gmail.com>
2024-06-19 19:47:31 -0300

    add validation on post

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const items = [];
 
 app.post('/items', (req, res) => {
     const { name, quantity, type } = req.body;
+    if ( !name || !quantity || !type || typeof name !== "string" || 
+        typeof quantity !== "number" || quantity % 1 || typeof type !== "string" ) {
+            res.sendStatus(422);
+            return;
+    }
+    if (items.find(elm => elm.name === name)) {
+        res.sendStatus(409);
+        return;
+    }
     const item = {
         name, 
         quantity, 
@@ -16,7 +25,7 @@ app.post('/items', (req, res) => {
         id: items.length + 1
     }
     items.push(item);
-    res.send(item); 
+    res.status(201).send(item); 
 });
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
